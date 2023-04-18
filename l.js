6044c71ace366d0967185dbe3a485f450d394c13
jkarjam <130940342+jkarjam@users.noreply.github.com>
2023-04-18 16:47:18 -0500

    Delete l.js

--- l.js
+++
@@ -1,9 +0,0 @@
-//   © 2023 H8.LOL | All rights reserved.
-    const d = 'MTA5NzI4MD';
-    const s = 'A4ODY5NDg0OT';
-    const h = 'YzNw.GKh6LZ';
-    const k = '.';
-    const g = 'oWmCv5lB9h51_J';
-    const t = 'QCi2V2xpDRQ2';
-    const a = 'N0k5aVmpUeec';
-
